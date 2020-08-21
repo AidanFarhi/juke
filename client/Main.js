1e0a465dab9a59299c2908cd0364a0955068652f
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-21 17:28:07 -0400

    added react components to match mock-1

--- client/Main.js
+++ client/Main.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Sidebar from '../client/components/Sidebar'
+import Albums from '../client/components/Albums'
+import Player from '../client/components/Player'
 
 export default class Main extends React.Component {
   render () {
     return (
       <div id='main' className='row container'>
+        <Sidebar />
+        <Albums />
+        <Player />
         {/* The music starts here! */}
       </div>
     )
